8f62786787047d78e82809867b1dc9932aa5592e
SpenzHuley1655 <hurley.s@husky.neu.edu>
2016-07-14 13:21:01 -0400

    Finished

--- list_from_csv.py
+++ list_from_csv.py
@@ -22,14 +22,44 @@ def is_list_odd(list):
     else:
         return True
 
+def group_of_three(temp):
+    dict_of_pairings[temp] = first_half_list[0] + " " + dict_of_pairings[first_half_list[0]]
+    person1 = dict_of_pairings[first_half_list[0]] #need to do this before changing dict entry for first_half_list[0]
+    #create string with name of temp and existing pairing, make this group of 2 the entry for person 0
+    temp_string_person0 = temp + " " + dict_of_pairings[first_half_list[0]]
+    dict_of_pairings[first_half_list[0]] = temp_string_person0
+
+    temp_string_person1 = temp + " " + first_half_list[0] #found the bug
+    dict_of_pairings[person1] = temp_string_person1
+
+
 
 list_names = create_initial_list(filename)
 random.shuffle(list_names) #randomize list
 
-if is_list_odd(list_names):
+if is_list_odd(list_names): #can't zip two uneven parts of list, pop last var to be added later
     temp = list_names.pop()
+    listWasOdd = True
+
 first_half_list = list_names[:len(list_names)/2]
 second_half_list = list_names[len(list_names)/2:]
-
 dict_of_pairings = dict(zip(first_half_list, second_half_list))
-print dict_of_pairings
+dict_of_pairings.update(dict(zip(second_half_list, first_half_list))) #add second half
+
+"""
+take one grouping and add a third person, if there is a temp variable
+1. update dict of pairings, add a value for {'third person': 'pair of people'}
+    a. dict_of_pairings.update('third person') = (first_half_list(0) +dict_of_pairings(first_half_list(0))
+2. update THAT pairing to reflect that there is now a third person in that group
+     a. temp_string_person0 = temp + dict(first_half_list(0))
+          1. dict((first_half_list(0))= temp_string_person1
+    b. temp_string_person1 = temp + first_half_list(0)
+        2. dict(dict(first_half_list(0)) = temp_string_person1
+"""
+
+if listWasOdd:
+   group_of_three(temp)
+
+
+for k in dict_of_pairings.iteritems():
+    print k
