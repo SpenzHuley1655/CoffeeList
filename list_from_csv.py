fba03a3c523f3314a28922c535c7b6669de6cfd9
SpenzHuley1655 <hurley.s@husky.neu.edu>
2016-07-14 11:18:49 -0400

    first half of dict is completed

--- list_from_csv.py
+++ list_from_csv.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import random
-import numpy as np
 
 df = []
 filename = 'Employees_Birthday_Report_FINAL.csv'
@@ -15,10 +14,22 @@ def create_initial_list(filename):
     list_names = []  # initialize list
     for name in range(length):  # for every index of dataframe
         list_names.append(df.iloc[name]['Full Name'])  # append full names to list of full names
-    backuplist = list_names
     return list_names
-list_names = create_initial_list(filename)
-print list_names
-random.shuffle(list_names)
-print list_names
 
+def is_list_odd(list):
+    if len(list) % 2 == 0:
+        return False
+    else:
+        return True
+
+
+list_names = create_initial_list(filename)
+random.shuffle(list_names) #randomize list
+
+if is_list_odd(list_names):
+    temp = list_names.pop()
+first_half_list = list_names[:len(list_names)/2]
+second_half_list = list_names[len(list_names)/2:]
+
+dict_of_pairings = dict(zip(first_half_list, second_half_list))
+print dict_of_pairings
