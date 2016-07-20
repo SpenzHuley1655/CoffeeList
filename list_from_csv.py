6f84938206da397011d43414754eeea1b6fb53d5
SpenzHuley1655 <hurley.s@husky.neu.edu>
2016-07-20 15:56:58 -0400

    Write pairs to CSV

--- list_from_csv.py
+++ list_from_csv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import random
+import csv
 
-df = []
 filename = 'Employees_Birthday_Report_FINAL.csv'
-
+outputfilename = 'mycsvfile.csv'
 def create_initial_list(filename):
     """
     Inputs a .csv filename
@@ -11,13 +11,15 @@ def create_initial_list(filename):
     """
     df = pd.read_csv(filename)
     length = len(df.index)  # how many names are in the data file
+    index = df.index
     list_names = []  # initialize list
     for name in range(length):  # for every index of dataframe
         list_names.append(df.iloc[name]['Full Name'])  # append full names to list of full names
     return list_names
 
 def is_list_odd(list):
-    if len(list) % 2 == 0:
+
+    if len(list) % 2 == 0:#if even
         return False
     else:
         return True
@@ -33,9 +35,8 @@ def group_of_three(temp):
     dict_of_pairings[person1] = temp_string_person1
 
 
-
 list_names = create_initial_list(filename)
-random.shuffle(list_names) #randomize list
+random.shuffle(list_names)#randomize list
 
 if is_list_odd(list_names): #can't zip two uneven parts of list, pop last var to be added later
     temp = list_names.pop()
@@ -44,12 +45,14 @@ if is_list_odd(list_names): #can't zip two uneven parts of list, pop last var to
 first_half_list = list_names[:len(list_names)/2]
 second_half_list = list_names[len(list_names)/2:]
 dict_of_pairings = dict(zip(first_half_list, second_half_list))
-dict_of_pairings.update(dict(zip(second_half_list, first_half_list))) #add second half
+dict_of_pairings.update(dict(zip(second_half_list, first_half_list)))#add second half
 
 
 if listWasOdd:
    group_of_three(temp)
 
+def write_to_csv(outputfilename):
+    with open('mycsvfile.csv','a') as f: #write pairs to a row
+        w = csv.writer(f)
+        w.writerows(dict_of_pairings.items())
 
-for k in dict_of_pairings.iteritems():
-    print k
