7684bbb18bc6297160b6cbc88bff6018c0df08af
SpenzHuley1655 <hurley.s@husky.neu.edu>
2016-12-15 16:40:18 -0500

    Update mainprogram.py
    Updated framework for main function. Will add more to README.md

--- mainprogram.py
+++ mainprogram.py
@@ -6,7 +6,11 @@ from jsonClass import JSON
 # all of the smaller pieces of the program from different files.
 
 # from top to bottom, this program:
+
 # Makes a dict from a .csv file, pairing up people from different departments
+# Kevin, I'll need your part of the program (I believe it's TNY_COMMIT1.py)
+# to return a simple dict. Basically, I need to be able to call a function like
+# get_dict_from_csv(filename) and have it return a dict of pairings.
 # Uses each pairing as parameters to the following loop:
 
 # make a request to the Google Calendar API, with those 
@@ -15,8 +19,26 @@ from jsonClass import JSON
 # If the value returned by r.meeting is {}, also recognized by python as false,
 # try again with the current datetime+24 hours
 # repeat until you receive a response with actual entries for date and time
-
-# then, use the Google Calendar API to create an event on those two peoples 
-# calendars.
-
 # iterate through this process with every pair in the original dict of pairings
+
+# what this will look like:
+
+"""
+for key in dict_of_pairings: # <--- dict_of_pairings from Kevin's program
+  response = make_google_request(key, dict_of_pairings[key], current_date) #design make_google_request
+  r = JSON(response)
+  r.do_all()
+  while not r.meeting: # an empty dict corresponds to "false" in python. 
+    make_google_request(key, dict_of_pairings[key], next_weekday) #must design next_weekday
+    r = JSON(response)
+    r.do_all()
+    
+  # then, use the Google Calendar API to create an event on those two peoples 
+  # calendars.
+  make_event(r.email1, r.email2, r.meeting['start'], r.meeting['end']) #must design make_event
+  
+"""
+
+
+
+
